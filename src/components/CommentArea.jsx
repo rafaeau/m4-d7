61d21ecbaad1524721df785648421091910c67d0
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-11 16:50:52 +0100

    CommentArea converted to functional

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,21 +1,20 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import CommentList from './CommentList'
 import AddComment from './AddComment'
 import Loading from './Loading'
 import Error from './Error'
 
-class CommentArea extends Component {
+const CommentArea = ({ asin }) => {
 
-    state = {
-        comments: [],
-        isLoading: true,
-        isError: false
-    }
+    const [comments, setComments] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
 
-    componentDidUpdate = async (prevProps) => {
-        if (prevProps.asin !== this.props.asin) {
+    useEffect(() => {
+        const fetchComments = async () => {
+            setIsLoading(true)
             try {
-                let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
+                let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin, {
                     headers: {
                         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjFmMTRiYjUzZDAwMTViMTllZDciLCJpYXQiOjE2NDE4MjIyMjQsImV4cCI6MTY0MzAzMTgyNH0.FhBfi1iCANr52V4vFI9jP1bs_nduCbOa8Ym3h3DfF6Q'
                     }
@@ -23,28 +22,31 @@ class CommentArea extends Component {
                 console.log(response)
                 if (response.ok) {
                     let comments = await response.json()
-                    this.setState({ comments: comments, isLoading: false, isError: false })
+                    setComments(comments)
+                    setIsLoading(false)
+                    setIsError(false)
                 } else {
                     console.log('error')
-                    this.setState({ isLoading: false, isError: true })
+                    setIsLoading(false)
+                    setIsError(false)
                 }
             } catch (error) {
                 console.log(error)
-                this.setState({ isLoading: false, isError: true })
+                setIsLoading(false)
+                setIsError(false)
             }
         }
-    }
+        fetchComments()
+    }, [asin])
 
-    render() {
-        return (
-            <div>
-                {this.state.isLoading && <Loading />}
-                {this.state.isError && <Error />}
-                <AddComment asin={this.props.asin} />
-                <CommentList commentsToShow={this.state.comments} />
-            </div>
-        )
-    }
+    return (
+        <div>
+            {isLoading && <Loading />}
+            {isError && <Error />}
+            <AddComment asin={asin} />
+            <CommentList commentsToShow={comments} />
+        </div>
+    )
 }
 
 export default CommentArea
