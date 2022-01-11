c9ee9c7a0f88a1b4d6c8644a8916900662458075
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-11 16:25:04 +0100

    BookList converted to functional

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,53 +1,46 @@
-import React from 'react'
+import { useState } from 'react'
 import SingleBook from './SingleBook'
 import CommentArea from './CommentArea'
 import { Col, Container, Form, Row } from 'react-bootstrap'
 
-class BookList extends React.Component {
+const BookList = ({books}) => {
 
-    state = {
-        searchQuery: '',
-        clickedBook: null
-    }
+    const [searchQuery, setSearchQuery] = useState('')
+    const [clickedBook, setClickedBook] = useState(null)
 
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col md={9}>
-                        <Row>
-                            <Col>
-                                <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>Search</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Search here"
-                                        value={this.state.searchQuery}
-                                        onChange={e => this.setState({ searchQuery: e.target.value })}
-                                    />
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row>
-                            {
-                                this.props.books.filter(b => b.title.toLowerCase().includes(this.state.searchQuery)).map(b => (
-                                    <Col xs={3} key={b.asin} >
-                                        <SingleBook book={b} alterClickedBook={asin => this.setState({
-                                            clickedBook: asin
-                                        })} />
-                                    </Col>
-                                ))
-                            }
-                        </Row>
-                    </Col>
-                    <Col md={3}>
-                        <CommentArea asin={this.state.clickedBook} />
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
-
+    return (
+        <Container>
+            <Row>
+                <Col md={9}>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Search</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Search here"
+                                    value={searchQuery}
+                                    onChange={e => setSearchQuery(e.target.value)}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {
+                            books.filter(b => b.title.toLowerCase().includes(searchQuery)).map(b => (
+                                <Col xs={3} key={b.asin} >
+                                    <SingleBook book={b} alterClickedBook={asin => setClickedBook(asin)} />
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </Col>
+                <Col md={3}>
+                    <CommentArea asin={clickedBook} />
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default BookList
